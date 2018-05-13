bb5a73c386575b1cc11e757b1dd21751060c5d31
unknown <tharukasahan@hotmail.com>
2018-05-14 00:39:55 +1000

    Search Bar

--- search.php
+++ search.php
@@ -36,7 +36,7 @@ if (!isset($_SESSION['login_user'])) {
 
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<title><?php echo $title; ?></title>
 	<link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
